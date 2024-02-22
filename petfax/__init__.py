4234a60c646af0dc09053892ad6bbb1ec6f6e520
tamccullough88 <tamccullough88@gmail.com>
2024-02-21 19:44:17 -0500

    updated the init file with a new route

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -6,5 +6,15 @@ def create_app():
     @app.route('/')
     def hello(): 
         return 'Hello, PetFax!'
+    # pets index route
+    @app.route('/pets')
+    def pets(): 
+        return 'These are our pets available for adoption!'
+    
+    @app.route('/pets/adopt')
+    def adopt():
+        return 'This is where you can adopt a pet'
+
+
 
     return app
