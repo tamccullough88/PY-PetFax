1e4e13d2e1156fd8b60ede4b8376cf8b6e8cfa21
tamccullough88 <tamccullough88@gmail.com>
2024-02-26 15:08:53 -0500

    adding sql connection and setting up post route to send data to the database

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,15 +1,28 @@
 from flask import ( Blueprint, render_template, request, redirect ) 
+from . import models
+
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
 
-@bp.route('/', methods=['GET', 'POST'])
+@bp.route('/')
 def index(): 
-    if request.method == 'POST':
-        print(request.form)
-        return redirect('/facts')
-    
     return render_template('facts/index.html')
 
-@bp.route('/new')
-def new(): 
-    return render_template('facts/new_facts.html')
+@bp.route('/new', methods=["GET", "POST"])
+def new():
+    if request.method == "GET":
+        return render_template("facts/new_facts.html")
+    else:
+        submitter = request.form['submitter']
+        fact = request.form['fact']
+        new_fact = models.Fact(submitter=submitter, fact=fact)
+        print(new_fact)
+        models.db.session.add(new_fact)
+
+        models.db.session.commit()
+        print(request.form)
+        return redirect('/facts')
+        
+    
+
+
--- petfax/pet.py
+++ petfax/pet.py
@@ -2,7 +2,7 @@ from flask import (Blueprint, render_template )
 import json
 
 pets = json.load(open('pets.json'))
-print(pets)
+# print(pets)
 
 bp = Blueprint(
     'pet',
